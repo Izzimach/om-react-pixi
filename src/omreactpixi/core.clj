71b0dff3da841536af9b950436c55616cfe34dcd
Gary Haussmann <gjhaussmann@gmail.com>
2014-07-06 22:31:49 -0600

    Added component definitions

--- src/omreactpixi/core.clj
+++ src/omreactpixi/core.clj
@@ -2,4 +2,15 @@
 
 (ns omreactpixi.core)
 
+;; for some reason I made react-pixi components camelcase,
+;; so here we convert the names so that they are called as lowercase
+;; names in clojurescript
+
+(defmacro defn-pixi-element [tag]
+  (let [jsname  (symbol "js" (str "ReactPIXI." (name tag)))
+        cljname (symbol (clojure.string/lower-case (name tag)))]
+  `(defn ~cljname [opts# & children#]
+     (.apply ~jsname
+             nil  ;; context (javascript this)
+             (cljs.core/into-array (cons opts# children#))))))
 
--- src/omreactpixi/core.cljs
+++ src/omreactpixi/core.cljs
@@ -2,24 +2,43 @@
   (:require-macros [omreactpixi.core :as orp])
   (:require [goog.events :as events]
             [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [clojure.string :as string]))
 
 (def appstate (atom {:text "argh!"}))
 
-(defn text [opts & children]
-  (.apply js/ReactPIXI.Text nil (cljs.core/into-array (cons opts children))))
+;;
+;; need to make these more automated, om-style
+;;
+
+(orp/defn-pixi-element Stage)
+(orp/defn-pixi-element Text)
+(orp/defn-pixi-element BitmapText)
+(orp/defn-pixi-element Sprite)
+(orp/defn-pixi-element DisplayObjectContainer)
+(orp/defn-pixi-element TilingSprite)
+
+(defn autostage [cursor]
+  (let [opts #js {:width 400 :height 300}]
+    (reify
+      om/IRender
+      (render [_]
+              (stage opts (text #js {:x 100 :y 100 :key "ack" :text "Argadasdh!"})))
+      om/IDisplayName
+      (display-name [_] "Autostage"))))
 
 (defn stagewidget [cursor]
   (let [opts #js {:width 400 :height 300}]
     (reify
       om/IRender
       (render [_]
-              (js/ReactPIXI.Stage opts (text #js {:x 100 :y 100 :key "ack" :text "Argadasdh!"})))
+              (stage opts (text #js {:x 100 :y 100 :key "ack" :text "Argadasdh!"})))
       om/IDisplayName
-      (display-name [_] "StageWidget"))))
+      (display-name [_] "Stagewidget"))))
+
 
 (defn starthelloworld [appstate elementid]
-  (om/root stagewidget appstate
+  (om/root autostage appstate
            {:target (.getElementById js/document elementid)}))
 
 
