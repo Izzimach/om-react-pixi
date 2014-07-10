d3d396bfdba000462e60b813370a8de10cc8919e
Gary Haussmann <gjhaussmann@gmail.com>
2014-07-10 16:48:09 -0600

    Removed autogenerated js files

--- om-react-pixi.js
+++
@@ -1,5 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
-goog.addDependency("../om/dom.js", ['om.dom'], ['cljs.core']);
-goog.addDependency("../om/core.js", ['om.core'], ['cljs.core', 'om.dom', 'goog.ui.IdGenerator']);
-goog.addDependency("../omreactpixi/core.js", ['omreactpixi.core'], ['cljs.core', 'om.core', 'om.dom', 'goog.events']);
