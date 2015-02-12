41e4197f5c1f90af7ac9067d21f0ba4f1245a761
Gary Haussmann <gjhaussmann@gmail.com>
2015-02-11 22:08:15 -0700

    Make lollipops whirl around cuz why not

--- dev-src/examples/interactive/interactive.cljs
+++ dev-src/examples/interactive/interactive.cljs
@@ -68,6 +68,21 @@
                        {:x 0 :y 0}
                        (om/build-all clickablesprite (vals sprites) {:key :key :fn bindimageandchannel})))))
 
+(defcomponentk spinelement [[:data x y spinspeed spinelement] owner]
+  (display-name [_] "SpinElement")
+  (init-state [_] {:clusterrotation 0 :spincallback nil})
+  (did-mount [_] (let [spinfn (fn spincallback [_] (let [oldrotation (om/get-state owner :clusterrotation)
+                                                         rotationdelta (* 0.016 spinspeed)
+                                                         newrotation (+ oldrotation rotationdelta)]
+                                                     (om/set-state! owner :clusterrotation newrotation)
+                                                     (om/set-state! owner :spincallback (js/requestAnimationFrame spincallback))))]
+                   (om/set-state! owner :spincallback (js/requestAnimationFrame spinfn))))
+  (will-unmount [_] (when-let [spinfn (om/get-state owner :spincallback)]
+                      (js/cancelAnimationFrame spinfn)))
+  (render-state [_ state] (let [r (:clusterrotation state)]
+                (pixi/displayobjectcontainer {:x x :y y :rotation r}
+                                             (pixi/displayobjectcontainer {:x (- x) :y (- y)}
+                                                                          spinelement)))))
 
 (defcomponentk interactivestage [[:data width height sprites :as props ] owner]
   (display-name [_] "InteractiveStage")
@@ -86,12 +101,16 @@
                 (let [addchannel (:addspritechannel state)
                       deletechannel (:deletespritechannel state)
                       fontstyle {:font "40 Comic_Neue_Angular"}
-                      fonttint 16rff88ff88]
+                      fonttint 16rff88ff88
+                      halfheight (/ height 2)
+                      halfwidth (/ width 2)
+                      spritecluster (om/build removeablesprites {:sprites sprites :deletechannel (:deletespritechannel state)})]
                   (pixi/stage {:width width :height height :interactive true}
                               (pixi/bitmaptext {:text "Click the cherry to add a lollipop" :x 10 :y 10 :tint fonttint :style fontstyle :key "text1"})
                               (pixi/bitmaptext {:text "Click a lollipop to remove it" :x 10 :y 60 :tint fonttint :style fontstyle :key "text2"})
                               (om/build clickablesprite {:spritedata {:x 100 :y 150 :key "addbutton" :interactive true :image (assetpath "cherry.png")} :clickchannel (:addspritechannel state)})
-                              (om/build removeablesprites {:sprites sprites :deletechannel (:deletespritechannel state)})))))
+                              (om/build spinelement {:spinelement spritecluster :x halfwidth :y halfheight :spinspeed 3})
+                              ))))
 
 ;;
 ;; entry/start code
